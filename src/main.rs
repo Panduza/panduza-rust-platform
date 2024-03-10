24452f10463d12bb4a3f5e8eaae25fb21d0da4d6
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-03-10 20:45:46 +0100

    up

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use tokio::sync::mpsc;
 
 use tokio::time::{sleep, Duration};
 
-use tokio::task::JoinSet;
 
 
 use tracing_subscriber::fmt::format::FmtSpan;
@@ -21,124 +20,13 @@ use std::error::Error;
 use std::cell::RefCell;
 
 
-#[derive(Debug)]
-struct Platform
-{
-    tasks_group: JoinSet<()>,
-    tasks_group_: RefCell<JoinSet<()>>
-}
-
-
-impl Platform {
-
-    fn new() -> Platform {
-        return Platform {
-            tasks_group: JoinSet::new(),
-            tasks_group_: RefCell::<JoinSet<()>>::new(JoinSet::new())
-        }
-    }
-
-    #[tracing::instrument(level = "info")]
-    async fn sleepy_task(i:u8) {
-        
-        for number in 0..=10 {
-            sleep(Duration::from_millis(1000)).await;
-            tracing::warn!("{i} have elapsed {number}");
-        }
-    }
-
-    #[tracing::instrument]
-    async fn task_waiter( &mut self) {
-
-        while let Some(result) = self.tasks_group.join_next().await {
-            println!("End task ");
-        }
-    }
-
-    #[tracing::instrument]
-    async fn test(&mut self) {
-        println!("pok");
-    }
-    // async fn test(&self) {
-    //     println!("pok");
-    // }
-
-    #[tracing::instrument]
-    pub async fn run(&mut self) {
-        println!("Hello, world!");
-
-
-        // Create a channel with a capacity of 10 messages
-        // let (tx, rx): (Sender<(i32, usize)>, Receiver<(i32, usize)>) = channel();
-
-
-        let ff = RefCell::<u32>::new(5);
-        
-        *ff.borrow_mut() = 9;
-        println!("{}", *ff.borrow());
-
-
-        let newww = ff.clone();
-
-        // let fut = ;
-        self.tasks_group.spawn(Platform::sleepy_task(1));
-        self.tasks_group.spawn(Platform::sleepy_task(2));
-
-        self.tasks_group_.borrow_mut().spawn(Platform::sleepy_task(3));
-
-        // let tttttt = self.tasks_group_.clone();
-
-        // let ppp = self.tasks_group.clone();
-
-
-        let mut mqttoptions = MqttOptions::new("rumqtt-async", "localhost", 1883);
-        mqttoptions.set_keep_alive(Duration::from_secs(5));
-
-        let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-        client.subscribe("hello/rumqtt", QoS::AtMostOnce).await.unwrap();
-
-
-        self.tasks_group.spawn(async move {
-
-
-            // tttttt.borrow_mut().spawn(Platform::sleepy_task(3));
-
-            for i in 0..10 {
-                println!("{}", *newww.borrow());
-                client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
-                time::sleep(Duration::from_millis(100)).await;
-            }
-        });
-
-        self.tasks_group.spawn(async move {
-            while let Ok(notification) = eventloop.poll().await {
-                println!("Received = {:?}", notification);
-            }
-        });
-
-
-
-        // loop {} ???
-        tokio::select! {
-            _ = signal::ctrl_c() => {
-                println!("end by user ctl-c");
-                
-            },
-            // _ = shutdown_recv.recv() => {},
-            _ = self.task_waiter() => {
-                println!("end of all");
-            
-            }
-        }
-    
-    }
-}
 
 
 
 mod device;
 
 
+mod platform;
 
 
 #[tokio::main]
@@ -146,40 +34,40 @@ async fn main() {
 
     
     
-    let mut dv = device::Factory::new();
+    // let mut dv = device::Factory::new();
 
-    dv.add_producer("ddd".to_string(), Box::new(device::CustommmProducer{}) );
+    // dv.add_producer("ddd".to_string(), Box::new(device::CustommmProducer{}) );
 
-    // dv.get_producer("ddd".to_string()).create_device();
+    // // dv.get_producer("ddd".to_string()).create_device();
 
-    let ddddd = "ddd".to_string();
-    let bbbb = dv.create_device( &ddddd ).unwrap();
+    // let ddddd = "ddd".to_string();
+    // let bbbb = dv.create_device( &ddddd ).unwrap();
     
 
-    println!("{}", bbbb.get_name());
+    // println!("{}", bbbb.get_name());
 
 
-    let subscriber = tracing_subscriber::fmt()
-    // Use a more compact, abbreviated log format
-    .compact()
-    // Display source code file paths
-    .with_file(true)
-    // Display source code line numbers
-    .with_line_number(true)
-    // Display the thread ID an event was recorded on
-    .with_thread_ids(true)
-    // Don't display the event's target (module path)
-    .with_target(false)
-    // .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
-    .with_span_events(FmtSpan::FULL)
-    // Build the subscriber
-    .finish();
+    // let subscriber = tracing_subscriber::fmt()
+    // // Use a more compact, abbreviated log format
+    // .compact()
+    // // Display source code file paths
+    // .with_file(true)
+    // // Display source code line numbers
+    // .with_line_number(true)
+    // // Display the thread ID an event was recorded on
+    // .with_thread_ids(true)
+    // // Don't display the event's target (module path)
+    // .with_target(false)
+    // // .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+    // .with_span_events(FmtSpan::FULL)
+    // // Build the subscriber
+    // .finish();
 
-    // use that subscriber to process traces emitted after this point
-    tracing::subscriber::set_global_default(subscriber).unwrap();
+    // // use that subscriber to process traces emitted after this point
+    // tracing::subscriber::set_global_default(subscriber).unwrap();
 
     
-    // console_subscriber::init();
+    // // console_subscriber::init();
 
 
 
@@ -190,9 +78,9 @@ async fn main() {
 
 
 
-    let mut platform = Platform::new();
+    let mut platform_runner = platform::Runner::new();
 
-    platform.run().await;
+    platform_runner.work().await;
 
     // let (shutdown_send, mut shutdown_recv) = mpsc::unbounded_channel::<u8>();
 
---
+++ src/platform.rs
@@ -0,0 +1,119 @@
+
+use tokio::task::JoinSet;
+
+
+
+pub struct Runner
+{
+    tasks: JoinSet<()>
+}
+
+
+impl Runner {
+
+
+
+    pub fn new() -> Runner {
+        return Runner {
+            tasks: JoinSet::new()
+        }
+    }
+
+    // #[tracing::instrument(level = "info")]
+    // async fn sleepy_task(i:u8) {
+        
+    //     for number in 0..=10 {
+    //         sleep(Duration::from_millis(1000)).await;
+    //         tracing::warn!("{i} have elapsed {number}");
+    //     }
+    // }
+
+    // #[tracing::instrument]
+    // async fn task_waiter( &mut self) {
+
+    //     while let Some(result) = self.tasks.join_next().await {
+    //         println!("End task ");
+    //     }
+    // }
+
+    // #[tracing::instrument]
+    // async fn test(&mut self) {
+    //     println!("pok");
+    // }
+    // // async fn test(&self) {
+    // //     println!("pok");
+    // // }
+
+    // #[tracing::instrument]
+    pub async fn work(&mut self) {
+        println!("Hello, world!");
+
+    }
+
+
+    //     // Create a channel with a capacity of 10 messages
+    //     // let (tx, rx): (Sender<(i32, usize)>, Receiver<(i32, usize)>) = channel();
+
+
+    //     let ff = RefCell::<u32>::new(5);
+        
+    //     *ff.borrow_mut() = 9;
+    //     println!("{}", *ff.borrow());
+
+
+    //     let newww = ff.clone();
+
+    //     // let fut = ;
+    //     self.tasks.spawn(Platform::sleepy_task(1));
+    //     self.tasks.spawn(Platform::sleepy_task(2));
+
+    //     self.tasks_group_.borrow_mut().spawn(Platform::sleepy_task(3));
+
+    //     // let tttttt = self.tasks_group_.clone();
+
+    //     // let ppp = self.tasks.clone();
+
+
+    //     let mut mqttoptions = MqttOptions::new("rumqtt-async", "localhost", 1883);
+    //     mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    //     let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    //     client.subscribe("hello/rumqtt", QoS::AtMostOnce).await.unwrap();
+
+
+    //     self.tasks.spawn(async move {
+
+
+    //         // tttttt.borrow_mut().spawn(Platform::sleepy_task(3));
+
+    //         for i in 0..10 {
+    //             println!("{}", *newww.borrow());
+    //             client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+    //             time::sleep(Duration::from_millis(100)).await;
+    //         }
+    //     });
+
+    //     self.tasks.spawn(async move {
+    //         while let Ok(notification) = eventloop.poll().await {
+    //             println!("Received = {:?}", notification);
+    //         }
+    //     });
+
+
+
+    //     // loop {} ???
+    //     tokio::select! {
+    //         _ = signal::ctrl_c() => {
+    //             println!("end by user ctl-c");
+                
+    //         },
+    //         // _ = shutdown_recv.recv() => {},
+    //         _ = self.task_waiter() => {
+    //             println!("end of all");
+            
+    //         }
+    //     }
+    
+    // }
+}
+
