968795af488c10ae3b217f01f0c568c3385ee089
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-03-12 15:41:27 +0100

    up

--- src/interfaces/mod.rs
+++ src/interfaces/mod.rs
@@ -18,20 +18,51 @@ enum States {
     // conn down
 // error
 trait StateCallbacks {
-    
 
 }
 
 
 struct Interface {
-    state: State 
-    // states definition
+    state: State,
+    callbacks: StateCallbacks
 }
 
 
 impl Interface {
+
     pub fn new() -> Interface {
         Interface {}
     }
+
+    /// Function to start the work in a task
+    /// 
+    pub async fn start(&mut self, task_pool: &mut tokio::task::JoinSet<()>) {
+        let abort = task_pool.spawn(self.work());
+    }
+
+    ///
+    pub async fn work(&mut self) {
+
+        loop {
+            
+            tracing::info!("Starting interface");
+
+            match self.state {
+                States::NotConnected => {
+                    // wait for connection
+                },
+                States::Initializing => {
+                    // wait for init
+                },
+                States::Running => {
+                    // wait for error
+                },
+                States::Error => {
+                    // wait for connection
+                }
+            }
+        }
+    }
+
 }
 
