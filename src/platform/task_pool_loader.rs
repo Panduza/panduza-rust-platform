08b7558f558446fe45b43223973282681b8a3559
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-04-02 08:33:40 +0200

    up

--- src/platform/task_pool_loader.rs
+++ src/platform/task_pool_loader.rs
@@ -1,30 +1,31 @@
 use std::pin::Pin;
-use futures::FutureExt;
-
-use futures::future::BoxFuture;
 use futures::Future;
 
-
+use super::error;
+use super::platform_error;
 use super::PlatformTaskResult;
 
-use super::platform_error;
-use super::error;
-
+/// Task pool loader
+/// 
 #[derive(Clone)]
 pub struct TaskPoolLoader {
-
+    /// Task pool sender
+    /// To send tasks to the task pool
     task_pool_tx: tokio::sync::mpsc::Sender<Pin<Box<dyn Future<Output = PlatformTaskResult> + Send>>>
-
 }
 
 impl TaskPoolLoader {
 
+    /// Create a new task pool loader
+    /// 
     pub fn new(tx: tokio::sync::mpsc::Sender<Pin<Box<dyn Future<Output = PlatformTaskResult> + Send>>>) -> TaskPoolLoader {
         return TaskPoolLoader {
             task_pool_tx: tx
         }
     }
 
+    /// Load a future into the task pool
+    /// 
     pub fn load(&mut self, future: Pin<Box<dyn Future<Output = PlatformTaskResult> + Send>>) -> Result<(), error::PlatformError>{
         let r = self.task_pool_tx.try_send(future);
         match r {
@@ -32,7 +33,8 @@ impl TaskPoolLoader {
                 return Ok(());
             },
             Err(e) => {
-                return platform_error!("Failed to send task to task pool", None);
+                return platform_error!(
+                    format!("Failed to send task to task pool: {}", e), None);
             }
         }
     }
