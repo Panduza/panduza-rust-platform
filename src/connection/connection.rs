c53a31742b9e3f5c9f4542725f878fda919710bb
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-05-16 07:30:55 +0200

    move on

--- src/connection/connection.rs
+++ src/connection/connection.rs
@@ -76,64 +76,16 @@ impl Connection {
     /// \todo: rename connection_task and move it outisde of the connection impl block
     /// \todo: pass as parameter the connection object inside of all its components (connection will be clonable)
     /// 
-    async fn run(
-        conneciton_name: String,
-        ev: Arc<Mutex<rumqttc::EventLoop>>,
-        lm: Arc<Mutex<LinkManager>>)
-        -> PlatformTaskResult {
+    // async fn run(
+    //     conneciton_name: String,
+    //     ev: Arc<Mutex<rumqttc::EventLoop>>,
+    //     lm: Arc<Mutex<LinkManager>>)
+    //     -> PlatformTaskResult {
 
-        // Event loop mangement
-        // Poll the reception in a loop
-        loop {
-            while let Ok(notification) = ev.lock().await.poll().await {
-                // Debug log
-                tracing::trace!(class="Connection", cname=conneciton_name,
-                    "{:?}", notification);
 
-                // Check notification
-                match notification {
-                    rumqttc::Event::Incoming(incoming) => {
-                        Connection::process_incoming_packet(lm.clone(), &incoming).await;
-                    },
-                    // rumqttc::Event::Outgoing(outgoing) => {
-                    //     match outgoing {
-                    //         rumqttc::Outgoing::Subscribe(subscribe) => {
-                    //             println!("Subscribe = {:?}", subscribe);
-                    //         },
-                    //         _ => {
-                    //             // println!("Outgoing = {:?}", outgoing);
-                    //         }
-                    //         // rumqttc::Outgoing::Publish(_) => todo!(),
-                    //         // rumqttc::Outgoing::Unsubscribe(_) => todo!(),
-                    //         // rumqttc::Outgoing::PubAck(_) => todo!(),
-                    //         // rumqttc::Outgoing::PubRec(_) => todo!(),
-                    //         // rumqttc::Outgoing::PubRel(_) => todo!(),
-                    //         // rumqttc::Outgoing::PubComp(_) => todo!(),
-                    //         // rumqttc::Outgoing::PingReq => todo!(),
-                    //         // rumqttc::Outgoing::PingResp => todo!(),
-                    //         // rumqttc::Outgoing::Disconnect => todo!(),
-                    //         // rumqttc::Outgoing::AwaitAck(_) => todo!(),
-                    //     }
-                    //     // println!("Outgoing = {:?}", outgoing);
-                    // }
-                    _ => {
-                        // println!("Received = {:?}", notification);
-                    }
-                }
+    //     }
 
-                // \todo: check for link manager events and initialize the new created links
-            }
-
-            // Here the broker is disconnected
-            tracing::warn!(class="Connection", cname=conneciton_name,
-                "Broker disconnected, trying to reconnect");
-
-            let message = 
-                                subscription::Message::new_connection_status(false);
-            // let r = link.tx.send(message).await;
-        }
-
-    }
+    // }
 
     /// Process incoming packets
     /// 
--- src/connection/task.rs
+++ src/connection/task.rs
@@ -2,12 +2,63 @@ use super::ThreadSafeConnection;
 use crate::platform::TaskResult;
 
 /// Run the connection
-/// 
-/// \todo: pass as parameter the connection object inside of all its components (connection will be clonable)
-/// 
-async fn task(connection: ThreadSafeConnection) -> TaskResult
-{
+///
+async fn task(connection: ThreadSafeConnection) -> TaskResult {
+    // Event loop mangement
+    // Poll the reception in a loop
+    loop {
+        while let Ok(notification) = ev.lock().await.poll().await {
+            // Debug log
+            tracing::trace!(
+                class = "Connection",
+                cname = conneciton_name,
+                "{:?}",
+                notification
+            );
 
+            // Check notification
+            match notification {
+                rumqttc::Event::Incoming(incoming) => {
+                    Connection::process_incoming_packet(lm.clone(), &incoming).await;
+                }
+                // rumqttc::Event::Outgoing(outgoing) => {
+                //     match outgoing {
+                //         rumqttc::Outgoing::Subscribe(subscribe) => {
+                //             println!("Subscribe = {:?}", subscribe);
+                //         },
+                //         _ => {
+                //             // println!("Outgoing = {:?}", outgoing);
+                //         }
+                //         // rumqttc::Outgoing::Publish(_) => todo!(),
+                //         // rumqttc::Outgoing::Unsubscribe(_) => todo!(),
+                //         // rumqttc::Outgoing::PubAck(_) => todo!(),
+                //         // rumqttc::Outgoing::PubRec(_) => todo!(),
+                //         // rumqttc::Outgoing::PubRel(_) => todo!(),
+                //         // rumqttc::Outgoing::PubComp(_) => todo!(),
+                //         // rumqttc::Outgoing::PingReq => todo!(),
+                //         // rumqttc::Outgoing::PingResp => todo!(),
+                //         // rumqttc::Outgoing::Disconnect => todo!(),
+                //         // rumqttc::Outgoing::AwaitAck(_) => todo!(),
+                //     }
+                //     // println!("Outgoing = {:?}", outgoing);
+                // }
+                _ => {
+                    // println!("Received = {:?}", notification);
+                }
+            }
+
+            // \todo: check for link manager events and initialize the new created links
+        }
+
+        // Here the broker is disconnected
+        tracing::warn!(
+            class = "Connection",
+            cname = conneciton_name,
+            "Broker disconnected, trying to reconnect"
+        );
+
+        let message = subscription::Message::new_connection_status(false);
+        // let r = link.tx.send(message).await;
+    }
     Ok(())
 }
-
