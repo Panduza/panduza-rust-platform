04c9e984e66bc792204ad9f3fe4cda820e0ca463
Paul FISHER <paul.fisher@elsys-design.com>
2024-05-29 14:31:06 +0200

    display device info

--- src/device/manager.rs
+++ src/device/manager.rs
@@ -61,8 +61,8 @@ impl Manager {
     ///
     pub async fn create_device(&mut self, device_def: &serde_json::Value) -> Result<String, PlatformError> {
         // Debug log
-        tracing::debug!(class="Platform", " - Try to create device -\n{}", serde_json::to_string_pretty(&device_def).unwrap() );
-
+        tracing::debug!(class="Platform", " - Try to create device -\n{}", serde_json::to_string_pretty(&device_def).unwrap());
+        println!("``` \n{}\n ```",serde_json::to_string_pretty(&device_def).unwrap());
         // Create the device
         let result = self.factory.create_device(device_def);
         match result {
--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -412,7 +412,8 @@ impl Platform {
             Ok(json) => {
                 // log
                 tracing::info!(class="Platform", " - Tree Json content -\n{}", serde_json::to_string_pretty(&json).unwrap());
-
+                println!("``` \n{}\n ```",serde_json::to_string_pretty(&json).unwrap());
+            
                 services.lock().await.set_tree_content(json);
 
                 return Ok(());
