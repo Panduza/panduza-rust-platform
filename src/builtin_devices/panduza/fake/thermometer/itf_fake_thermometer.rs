71c7fbea37489fb401e2ea686677fd2f5831f7d1
Lucas-D20 <lucas.giese@elsys-design.com>
2024-06-06 14:00:26 +0200

    orthographe

--- src/builtin_devices/panduza/fake/thermometer/itf_fake_thermometer.rs
+++ src/builtin_devices/panduza/fake/thermometer/itf_fake_thermometer.rs
@@ -9,25 +9,24 @@ use crate::interface::builder::Builder as InterfaceBuilder;
 /// Fake Thermometer Channel Data
 /// 
 struct FakeThermometerActions {
-    mesure_value: f64,
+    measure_value: f64,
 }
 
 #[async_trait]
 impl thermometer::ThermometerActions for FakeThermometerActions {
-
     /// Initialize the interface
     /// 
     async fn initializating(&mut self, _interface: &AmInterface) -> Result<(), PlatformError> {
         return Ok(());
     }
 
-    /// Read the mesure value
+    /// Read the measure value
     /// 
-    async fn read_mesure_value(&mut self, interface: &AmInterface) -> Result<f64, PlatformError> {
+    async fn read_measure_value(&mut self, interface: &AmInterface) -> Result<f64, PlatformError> {
         interface.lock().await.log_info(
-            format!("FakeThermometer - read_mesure_value: {}", self.mesure_value)
+            format!("FakeThermometer - read_measure_value: {}", self.measure_value)
         );
-        return Ok(self.mesure_value);
+        return Ok(self.measure_value);
     }
 }
 
@@ -42,10 +41,10 @@ pub fn build<A: Into<String>>(
     return thermometer::build(
         name, 
         thermometer::ThermometerParams {
-            mesure_decimals: 3
+            measure_decimals: 3
         },
         Box::new(FakeThermometerActions {
-            mesure_value: 0.0,
+            measure_value: 0.0,
             
         })
     )
