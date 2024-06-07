a983399ef402200893caabc541a6b8327ddebfc9
Lucas-D20 <lucas.giese@elsys-design.com>
2024-06-07 15:01:52 +0200

    remove multiple occurence of producer

--- src/builtin_devices/panduza/fake/mod.rs
+++ src/builtin_devices/panduza/fake/mod.rs
@@ -12,9 +12,8 @@ pub fn import_plugin_producers(factory: &mut DeviceFactory)
 
     factory.add_producer("panduza.fake_power_supply", Box::new(power_supply::DeviceProducer{}));
     factory.add_producer("panduza.fake_thermometer", Box::new(thermometer::DeviceProducer{}));
-    factory.add_producer("panduza.fake_laser", Box::new(laser::DeviceProducer{}));
-    factory.add_producer("panduza.fake_thermometer", Box::new(thermometer::DeviceProducer{}));
     factory.add_producer("panduza.fake_powermeter", Box::new(powermeter::DeviceProducer{}));
+    factory.add_producer("panduza.fake_laser", Box::new(laser::DeviceProducer{}));
 
 }
 
