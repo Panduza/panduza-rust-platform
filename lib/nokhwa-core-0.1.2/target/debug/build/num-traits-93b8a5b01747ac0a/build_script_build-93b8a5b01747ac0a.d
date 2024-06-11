3bf91f40a277cf54c13e17e2beead95f8eb9c46c
Damien Albisson <UF205DAL@net.elsys-design.com>
2024-06-11 15:02:51 +0200

    add every lib need to not have to change the crate

---
+++ lib/nokhwa-core-0.1.2/target/debug/build/num-traits-93b8a5b01747ac0a/build_script_build-93b8a5b01747ac0a.d
@@ -0,0 +1,5 @@
+C:\Users\UF205DAL\.cargo\registry\src\index.crates.io-6f17d22bba15001f\nokhwa-core-0.1.2\target\debug\build\num-traits-93b8a5b01747ac0a\build_script_build-93b8a5b01747ac0a.exe: C:\Users\UF205DAL\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs
+
+C:\Users\UF205DAL\.cargo\registry\src\index.crates.io-6f17d22bba15001f\nokhwa-core-0.1.2\target\debug\build\num-traits-93b8a5b01747ac0a\build_script_build-93b8a5b01747ac0a.d: C:\Users\UF205DAL\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs
+
+C:\Users\UF205DAL\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs:
