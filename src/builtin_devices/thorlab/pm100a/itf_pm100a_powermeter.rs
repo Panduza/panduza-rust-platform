32eac392b68dfffbc669b91ea7ac6e7f18acc260
Lucas-D20 <lucas.giese@elsys-design.com>
2024-06-11 17:19:09 +0200

    try to write_bulk to out endpoint

--- src/builtin_devices/thorlab/pm100a/itf_pm100a_powermeter.rs
+++ src/builtin_devices/thorlab/pm100a/itf_pm100a_powermeter.rs
@@ -1,6 +1,8 @@
 use async_trait::async_trait;
-use rust_usbtmc;
-use rust_usbtmc::instrument::Instrument;
+use rusb;
+use std::time::Duration;
+
+// use rust_usbtmc::instrument::Instrument;
 use crate::platform::PlatformError;
 use crate::meta::powermeter;
 use crate::interface::AmInterface;
@@ -13,8 +15,8 @@ use crate::interface::builder::Builder as InterfaceBuilder;
 // use crate::platform_error_result;
 
 
-static VID: u16 = 0x1313;
-static PID: u16 = 0x8079;
+// static VID: u16 = 0x1313;
+// static PID: u16 = 0x8079;
 
 
 ///
@@ -22,7 +24,7 @@ static PID: u16 = 0x8079;
 struct PM100APowermeterActions {
     // connector_tty: tty::TtyConnector,
     // serial_config: SerialConfig,
-    instrument: Instrument,
+    // instrument: Instrument,
     measure_value: f64,
     // time_lock_duration: Option<tokio::time::Duration>,
 }
@@ -34,28 +36,73 @@ impl powermeter::PowermeterActions for PM100APowermeterActions {
     /// 
     async fn initializating(&mut self, _interface: &AmInterface) -> Result<(), PlatformError> {
 
+        // let c = rusb::Context::new();
+        let d = rusb::devices();
+        // println!("{}", d.unwrap().len());
+
+        let ll = d.as_ref().unwrap();
+        for dev in ll.iter() {
+            println!("{:#04x}", dev.device_descriptor().unwrap().vendor_id());
+            if dev.device_descriptor().unwrap().vendor_id() == 0x1313 {
+                let usb_device = dev.open().unwrap();
+                let mut data: [u8; 64] = [0; 64]; // Initialize the array with 64 zeros
+                // Copy the string "*idn?" into the array
+                data[.."*idn?".len()].copy_from_slice("*idn?".as_bytes());
+                // Print the entire array (including null terminator) for demonstration purposesprintln!("{:?}", data);
+                let count = usb_device.write_bulk(0x02, &data, Duration::from_millis(100));
+                match count {
+                    Ok(v) => println!("usize {}", v),
+                    Err(e) => println!("Error: {}", e),
+                };
+                // println!("{}", count);
+                    // match dev.open() {
+                    //     Ok(h) => match h.read_languages(timeout) {
+                    //         Ok(l) => {
+                    //             if l.len() > 0 {
+                    //                 Some(UsbDevice {
+                    //                     handle: h,
+                    //                     language: l[0],
+                    //                     timeout,
+                    //                 })
+                    //             } else {
+                    //                 None
+                    //             }
+                    //         }
+                    //         Err(_) => None,
+                    //     },
+                    //     Err(_) => None,
+                    // }
+            }
+        }
+
+        
+        // for dev in d.unwrap() {
+        //     println!("{}", dev);
+        // }
+        
+
         // self.connector_tty = tty::get(&self.serial_config).await.unwrap();
         // self.connector_tty.init().await;
-        self.instrument = Instrument::new(VID, PID);
+        // self.instrument = Instrument::new(VID, PID);
 
-        println!("yooooo!");
+        // println!("yooooo!");
         // println!("Ask: {}", self.instrument.ask("*IDN?").unwrap());
-        self.measure_value = self.instrument.read().unwrap().parse::<f64>().unwrap();
-        println!("measure {}", self.measure_value);
+        // self.measure_value = self.instrument.read().unwrap().parse::<f64>().unwrap();
+        // println!("measure {}", self.measure_value);
 
         return Ok(());
     }
 
     /// Read the measure value
     /// 
-    async fn read_measure_value(&mut self, interface: &AmInterface) -> Result<f64, PlatformError> {
+    async fn read_measure_value(&mut self, _interface: &AmInterface) -> Result<f64, PlatformError> {
 
-        self.measure_value = self.instrument.read().unwrap().parse::<f64>().unwrap();
-        println!("measure {}", self.measure_value);
+        // self.measure_value = self.instrument.read().unwrap().parse::<f64>().unwrap();
+        // println!("measure {}", self.measure_value);
 
-        interface.lock().await.log_info(
-            format!("PM100A - read_measure_value: {}", self.measure_value)
-        );
+        // interface.lock().await.log_info(
+        //     format!("PM100A - read_measure_value: {}", self.measure_value)
+        // );
         return Ok(self.measure_value);
 
         // interface.lock().await.log_warn(
@@ -82,7 +129,7 @@ pub fn build<A: Into<String>>(
         Box::new(PM100APowermeterActions {
             // connector_tty: TtyConnector::new(None),
             // serial_config: serial_config.clone(),
-            instrument: Instrument::new(0, 0),
+            // instrument: Instrument::new(0, 0),
             measure_value: 0.0,
             // time_lock_duration: Some(tokio::time::Duration::from_millis(100)),
         })
--- src/connector/serial/usbtmc.rs
+++ src/connector/serial/usbtmc.rs
@@ -1,125 +1,169 @@
-use std::{collections::HashMap, sync::Arc};
+// use std::{collections::HashMap, sync::Arc};
 
-use tokio;
-use lazy_static::lazy_static;
+// use tokio;
+// use lazy_static::lazy_static;
 
-lazy_static! {
-    static ref GATE : tokio::sync::Mutex<Gate>
-        = tokio::sync::Mutex::new(Gate { instances: HashMap::new() });
-}
+// lazy_static! {
+//     static ref GATE : tokio::sync::Mutex<Gate>
+//         = tokio::sync::Mutex::new(Gate { instances: HashMap::new() });
+// }
 
-pub async fn get(config: &Config) -> Option<UsbtmcConnector> {
-    let mut gate = GATE.lock().await;
-    gate.get(config)
-}
-
-
-#[derive(Clone, Debug)]
-pub struct Config {
-    pub usb_vendor: Option<u16>,
-    pub usb_model: Option<u16>,
-    pub usb_serial: Option<String>,
-}
-
-impl Config {
-    pub fn new() -> Config {
-        Config {
-            usb_vendor: None,
-            usb_model: None,
-            usb_serial: None,
-        }
-    }
-
-    pub fn import_from_json_settings(&mut self, settings: &serde_json::Value) {
-
-        self.usb_vendor =
-            settings.get("usb_vendor")
-                .map(|v| v.as_str().unwrap().to_string().parse::<u16>().unwrap());
-
-        self.usb_model =
-            settings.get("usb_model")
-                .map(|v| v.as_str().unwrap().to_string().parse::<u16>().unwrap());
-
-        self.usb_serial =
-            settings.get("usb_serial")
-                .map(|v| v.as_str().unwrap().to_string());
-
-    }
-}
+// pub async fn get(config: &Config) -> Option<UsbtmcConnector> {
+//     let mut gate = GATE.lock().await;
+//     gate.get(config)
+// }
 
 
+// #[derive(Clone, Debug)]
+// pub struct Config {
+//     pub usb_vendor: Option<u16>,
+//     pub usb_model: Option<u16>,
+//     pub usb_serial: Option<String>,
+// }
 
-struct Gate {
-    instances: HashMap<String, UsbtmcConnector>
-}
+// impl Config {
+//     pub fn new() -> Config {
+//         Config {
+//             usb_vendor: None,
+//             usb_model: None,
+//             usb_serial: None,
+//         }
+//     }
 
-impl Gate {
+//     pub fn import_from_json_settings(&mut self, settings: &serde_json::Value) {
 
+//         self.usb_vendor =
+//             settings.get("usb_vendor")
+//                 .map(|v| v.as_str().unwrap().to_string().parse::<u16>().unwrap());
 
-    fn get(&mut self, config: &Config) -> Option<UsbtmcConnector> {
-        // First try to get the key
-        let key_string = Gate::generate_unique_key_from_config(config)?;
-        let key= key_string.as_str();
+//         self.usb_model =
+//             settings.get("usb_model")
+//                 .map(|v| v.as_str().unwrap().to_string().parse::<u16>().unwrap());
 
-        // if the instance is not found, it means that the port is not opened yet
-        if ! self.instances.contains_key(key) {
+//         self.usb_serial =
+//             settings.get("usb_serial")
+//                 .map(|v| v.as_str().unwrap().to_string());
 
-            // Create a new instance
-            let new_instance = UsbtmcConnector::new(Some(config.clone()));
-
-            // Save the instance
-            self.instances.insert(key.to_string(), new_instance.clone());
-            tracing::info!(class="Platform", "connector created");
-        }
-
-        // Try to find the instance
-        let instance = self.instances.get(key)?;
-
-        // Return the instance
-        Some(instance.clone())
-    }
-
-    /// Try to generate a unique key from the config
-    /// This key will be used to find back the tty connector
-    ///
-    fn generate_unique_key_from_config(config: &Config) -> Option<String> {
-        // Check if the usb vendor and model are provided
-        if let Some(k) = Some(format!("{}_{}", config.usb_vendor.unwrap(), config.usb_model.unwrap())) {
-            return Some(k.clone());
-        }
-
-        // Finally unable to generate a key with the config
-        return None;
-    }
-
-}
+//     }
+// }
 
 
 
-#[derive(Clone)]
-pub struct UsbtmcConnector {
-    core: Option<Arc<tokio::sync::Mutex<UsbtmcCore>>>,
-}
+// struct Gate {
+//     instances: HashMap<String, UsbtmcConnector>
+// }
 
-impl UsbtmcConnector {
+// impl Gate {
+
+
+//     fn get(&mut self, config: &Config) -> Option<UsbtmcConnector> {
+//         // First try to get the key
+//         let key_string = Gate::generate_unique_key_from_config(config)?;
+//         let key= key_string.as_str();
+
+//         // if the instance is not found, it means that the port is not opened yet
+//         if ! self.instances.contains_key(key) {
+
+//             // Create a new instance
+//             let new_instance = UsbtmcConnector::new(Some(config.clone()));
+
+//             // Save the instance
+//             self.instances.insert(key.to_string(), new_instance.clone());
+//             tracing::info!(class="Platform", "connector created");
+//         }
+
+//         // Try to find the instance
+//         let instance = self.instances.get(key)?;
+
+//         // Return the instance
+//         Some(instance.clone())
+//     }
+
+//     /// Try to generate a unique key from the config
+//     /// This key will be used to find back the tty connector
+//     ///
+//     fn generate_unique_key_from_config(config: &Config) -> Option<String> {
+//         // Check if the usb vendor and model are provided
+//         if let Some(k) = Some(format!("{}_{}", config.usb_vendor.unwrap(), config.usb_model.unwrap())) {
+//             return Some(k.clone());
+//         }
+
+//         // Finally unable to generate a key with the config
+//         return None;
+//     }
+
+// }
+
+
+
+// #[derive(Clone)]
+// pub struct UsbtmcConnector {
+//     core: Option<Arc<tokio::sync::Mutex<UsbtmcCore>>>,
+// }
+
+// impl UsbtmcConnector {
     
-    pub fn new(config: Option<Config>) -> UsbtmcConnector {
-        match config {
-            Some(config)    => {
-                UsbtmcConnector {
-                    core: Some(
-                        Arc::new(tokio::sync::Mutex::new(
-                            UsbtmcCore::new(config)))
-                    )
-                }
-            }
-            None            => {
-                UsbtmcConnector {
-                    core: None
-                }
-            }
-        }
-    }
+//     pub fn new(config: Option<Config>) -> UsbtmcConnector {
+//         match config {
+//             Some(config)    => {
+//                 UsbtmcConnector {
+//                     core: Some(
+//                         Arc::new(tokio::sync::Mutex::new(
+//                             UsbtmcCore::new(config)))
+//                     )
+//                 }
+//             }
+//             None            => {
+//                 UsbtmcConnector {
+//                     core: None
+//                 }
+//             }
+//         }
+//     }
+
+//     pub async fn init(&mut self) {
+//         self.core
+//             .as_ref()
+//             .unwrap()
+//             .lock()
+//             .await
+//             .init()
+//             .await;
+//     }
+
+//     pub async fn write(&mut self, command: &[u8],
+//         time_lock: Option<Duration>) 
+//             -> Result<usize> {
+//         self.core
+//             .as_ref()
+//             .unwrap()
+//             .lock()
+//             .await
+//             .write(command, time_lock)
+//             .await
+//     }
 
 
-}
+//     pub async fn write_then_read(&mut self, command: &[u8], response: &mut [u8],
+//         time_lock: Option<Duration>) 
+//             -> Result<usize> {
+//         self.core
+//             .as_ref()
+//             .unwrap()
+//             .lock()
+//             .await
+//             .write_then_read(command, response, time_lock)
+//             .await
+//     }
+
+// }
+
+
+
+
+// struct TimeLock {
+//     duration: tokio::time::Duration,
+//     t0: tokio::time::Instant
+// }
+
+
