69fab64bfbd73cace9c584bf76d7ab8cfd91954f
Lucas-D20 <lucas.giese@elsys-design.com>
2024-06-21 10:29:12 +0200

    rebase on main

--- src/builtin_devices/oxxius/lbx_488/itf_lbx_488_blc.rs
+++ src/builtin_devices/oxxius/lbx_488/itf_lbx_488_blc.rs
@@ -1,10 +1,10 @@
 use async_trait::async_trait;
-use crate::platform::PlatformError;
-use crate::meta::blc;
-use crate::interface::AmInterface;
-use crate::interface::builder::Builder as InterfaceBuilder;
 
 
+use panduza_core::Error as PlatformError;
+use panduza_core::interface::AmInterface;
+use panduza_core::interface::builder::Builder as InterfaceBuilder;
+use panduza_core::meta::blc;
 use panduza_connectors::usb::usb::{self, UsbConnector};
 use panduza_connectors::usb::usb::Config as UsbConfig;
 
