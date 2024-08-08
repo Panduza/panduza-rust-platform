2de4fb721a933fb6ccabceffcaa96856bbe14d6c
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-08-08 15:39:11 +0200

    codec for numbers

--- lib/core/src/codec/number.rs
+++ lib/core/src/codec/number.rs
@@ -1,10 +1,10 @@
 use std::fmt::Display;
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::{Error, MessageCodec};
 
-#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct NumberCodec {
     value: serde_json::Value,
 }
@@ -17,6 +17,32 @@ impl Into<NumberCodec> for u64 {
         return NumberCodec {
             value: serde_json::json!(self),
         };
+    }
+}
+
+///
+/// Do not use derive because we do not want { "value": true }
+/// But only true or false on the payload
+///
+impl Serialize for NumberCodec {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.value.serialize(serializer)
+    }
+}
+
+///
+/// See Serialize
+///
+impl<'de> Deserialize<'de> for NumberCodec {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = serde_json::Value::deserialize(deserializer)?;
+        Ok(NumberCodec { value })
     }
 }
 
