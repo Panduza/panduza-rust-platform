98fd180e49df9a416b3e7257b724406e9f19f39d
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-10-11 14:34:45 +0100

    update

--- src/platform.rs
+++ src/platform.rs
@@ -1,7 +1,7 @@
 use futures::FutureExt;
 use panduza_platform_core::{
-    create_task_channel, env, DeviceMonitor, Factory, ProductionOrder, Runtime, TaskReceiver,
-    TaskResult, TaskSender,
+    create_task_channel, env, DeviceMonitor, Factory, Notification, ProductionOrder, Runtime,
+    TaskReceiver, TaskResult, TaskSender,
 };
 use panduza_platform_core::{PlatformLogger, Reactor, ReactorSettings};
 use rumqttd::Broker;
@@ -9,14 +9,12 @@ use rumqttd::Config;
 use std::fs::File;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::time::Duration;
 use tokio::signal;
 use tokio::sync::mpsc::channel;
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::mpsc::Sender;
 use tokio::sync::{Mutex, Notify};
 use tokio::task::JoinSet;
-use tokio::time::sleep;
 
 use crate::device_tree::DeviceTree;
 use crate::plugins_manager::PluginsManager;
@@ -83,6 +81,13 @@ pub struct Platform {
     ///
     ///
     plugin_manager: PluginsManager,
+
+    // -- Device monitoring
+    ///
+    /// Notifications that comes from devices
+    /// They will help the underscore device to give informations to the user
+    ///
+    notifications: Arc<Mutex<Vec<Notification>>>,
 }
 
 impl Platform {
@@ -111,6 +116,8 @@ impl Platform {
 
             reactor: None,
             plugin_manager: PluginsManager::new(),
+
+            notifications: Arc::new(Mutex::new(Vec::new())),
         };
     }
 
@@ -119,49 +126,6 @@ impl Platform {
     pub async fn run(&mut self) {
         // Info log
         self.logger.info("Platform Version ...");
-
-        // TODO: should be done thorugh connection.json
-        // let settings = ReactorSettings::new("localhost", 1883, None);
-
-        // //
-        // let mut reactor = Reactor::new(settings);
-        // reactor.start(self.task_sender.clone()).unwrap();
-
-        // create the root device inside main task
-
-        // // Creation à la mano du device de gestion de la platform
-        // let (info_device_operations, info_pack) = InfoDevice::new();
-        // let (mut info_monitor, info_device) = DeviceMonitor::new(
-        //     reactor.clone(),
-        //     None, // this device will manage info_pack and cannot use it to boot like other devices
-        //     Box::new(info_device_operations),
-        //     ProductionOrder::new("_", "_"),
-        // );
-
-        // let mut info_device_clone = info_device.clone();
-        // self.task_sender
-        //     .spawn(
-        //         async move {
-        //             info_device_clone.run_fsm().await;
-        //             Ok(())
-        //         }
-        //         .boxed(),
-        //     )
-        //     .unwrap();
-
-        // self.task_sender
-        //     .spawn(
-        //         async move {
-        //             info_monitor.run().await;
-        //             Ok(())
-        //         }
-        //         .boxed(),
-        //     )
-        //     .unwrap();
-
-        //
-        // Start service task
-        // self.task_sender.spawn(service_task().boxed()).unwrap();
 
         //
         //
@@ -384,7 +348,7 @@ impl Platform {
         let mut broker = Broker::new(rumqttd_config);
 
         self.logger.info("Start Broker");
-        let jh = std::thread::spawn(move || {
+        let _jh = std::thread::spawn(move || {
             broker.start().unwrap();
             println!("BROKER STOPPPED !!!!!!!!!!!!!!!!!");
         });
@@ -435,7 +399,8 @@ impl Platform {
 
         //
         //
-        let mut factory = Factory::new();
+        // mut
+        let factory = Factory::new();
         // factory.add_producers(plugin_producers());
 
         //
@@ -506,186 +471,3 @@ impl Platform {
         let _res = self.plugin_manager.produce(po).unwrap();
     }
 }
-
-//         // Start the main service task directly
-//         // it acts as a idle task for the platform to avoid the platform to stop if no other task
-//         let s = self.services.clone();
-//         let d = self.devices.clone();
-//         let c = self.connection.clone();
-
-//         // Quick and dirty store init
-//         let store = d.lock().await.create_an_empty_store();
-//         s.lock().await.update_device_store(store);
-
-//         // Start the services task
-//         self.task_pool.spawn(async move {
-//             Platform::services_task(s, d, c).await
-//         });
-
-//         // Start local discovery at the start of the application
-//         let plbd_platform_services = self.services.clone();
-//         self.task_pool.spawn(
-//             local_broker_discovery_task(plbd_platform_services)
-//         );
-
-//     }
-
-//     /// Services task
-//     ///
-//     async fn services_task(services: AmServices, devices: device::AmManager, connection: connection::AmManager)
-//         -> PlatformTaskResult
-//     {
-//         let requests_change_notifier = services.lock().await.get_requests_change_notifier();
-//         loop {
-//             // Wait for an event
-//             requests_change_notifier.notified().await;
-//             tracing::trace!(class="Platform", "Services task notified");
-//             {
-//                 if services.lock().await.has_pending_requests() {
-
-//                     // --------------------------------------------------------
-//                     // --- BOOT ---
-//                     if services.lock().await.booting_requested() {
-
-//                         execute_service_boot(services.clone()).await?;
-
-//                         if let Err(e) = Platform::load_tree_file(services.clone()).await {
-//                             tracing::warn!(class="Platform", "Failed to load tree: {}", e);
-//                             tracing::warn!(class="Platform", "Continue with default configuration");
-//                         }
-
-//                         // Sart minimal connection and devices
-//                         Platform::boot_minimal_services(services.clone(), devices.clone(), connection.clone()).await;
-
-//                         // log
-//                         tracing::info!(class="Platform", "Boot Success!");
-//                     }
-
-//                     // --------------------------------------------------------
-//                     // --- RELOAD ---
-//                     if services.lock().await.reload_tree_requested() {
-//                         tracing::info!(class="Platform", "Reloading Configuration Tree...");
-
-//                         // Try to reload the tree
-//                         if let Err(e) = Platform::reload_tree(
-//                             services.clone(), devices.clone(), connection.clone()).await {
-//                             tracing::error!(class="Platform", "Failed to reload tree: {}", e);
-//                         }
-
-//                         tracing::info!(class="Platform", "Reloading Success!");
-//                     }
-
-//                     // --------------------------------------------------------
-//                     // --- HUNT ---
-//                     if services.lock().await.hunt_requested() {
-//                         if execute_service_hunt(services.clone(), devices.clone()).await.is_err() {
-//                             return __platform_error_result!("Failed to hunt");
-//                         }
-//                     }
-
-//                     // --------------------------------------------------------
-//                     // --- STOP ---
-//                     if services.lock().await.stop_requested() {
-
-//                         return Ok(());
-//                     }
-
-//                 }
-//             }
-//         }
-//     }
-
-//     /// Start the broker connection
-//     ///
-//     async fn start_broker_connection(services: AmServices, devices: device::AmManager, connection: connection::AmManager) {
-
-//         let sss = services.lock().await;
-//         let oci = sss.connection_info();
-
-//         if oci.is_none() {
-//             return;
-//         }
-
-//         let ci = oci.as_ref()
-//             .unwrap()
-//             .clone();
-
-//         connection.lock().await.start_connection(&ci.broker_addr, ci.broker_port).await;
-//         devices.lock().await.set_connection_link_manager(connection.lock().await.connection().unwrap().lock().await.link_manager());
-
-//     }
-
-//     /// Load the tree file from system into service data
-//     ///
-//     async fn load_tree_file(services: AmServices) -> Result<(), crate::Error> {
-
-//         // Get the tree file path
-//         let mut tree_file_path = PathBuf::from(dirs::public_dir().unwrap()).join("panduza").join("tree.json");
-//         match env::consts::OS {
-//             "linux" => {
-//                 tree_file_path = PathBuf::from("/etc/panduza/tree.json");
-//                 // println!("We are running linux!");
-//             }
-//             "windows" => {
-
-//             }
-//             _ => {
-//                 tracing::error!("Unsupported system!");
-//             }
-//         }
-//         tracing::info!(class="Platform", "Loading tree file: {:?}", tree_file_path);
-
-//         // Try to read the file content
-//         let file_content = tokio::fs::read_to_string(&tree_file_path).await;
-//         match file_content {
-//             Ok(content) => {
-//                 return Platform::load_tree_string(services.clone(), &content).await;
-//             },
-//             Err(e) => {
-//                 return __platform_error_result!(
-//                     format!("Failed to read {:?} file content: {}", tree_file_path, e))
-//             }
-//         }
-//     }
-
-// /// Load a tree string into service data
-// ///
-// async fn load_tree_string(services: AmServices, content: &String) -> Result<(), crate::Error> {
-//     // Parse the JSON content
-//     let json_content = serde_json::from_str::<serde_json::Value>(&content);
-//     match json_content {
-//         Ok(json) => {
-//             // log
-//             tracing::info!(
-//                 class = "Platform",
-//                 " - Tree Json content -\n{}",
-//                 serde_json::to_string_pretty(&json).unwrap()
-//             );
-//             services.lock().await.set_tree_content(json);
-
-//             return Ok(());
-//         }
-//         Err(e) => return __platform_error_result!(format!("Failed to parse JSON content: {}", e)),
-//     }
-// }
-
-//     /// Boot default connection and platform device
-//     ///
-//     async fn boot_minimal_services(services: AmServices, devices: device::AmManager, connection: connection::AmManager) {
-
-//         // Start the broker connection
-//         Platform::start_broker_connection(services.clone(), devices.clone(), connection.clone()).await;
-
-//         // Lock managers to create connection and device
-//         let mut d = devices.lock().await;
-
-//         // Server hostname
-//         let hostname = hostname::get().unwrap().to_string_lossy().to_string();
-
-//         // Create server device
-//         if let Err(e) = d.create_device(&json!({
-//             "name": hostname,
-//             "ref": "panduza.server"
-//         })).await {
-//             tracing::error!(class="Platform", "Failed to create device:\n{}", e);
-//         }
