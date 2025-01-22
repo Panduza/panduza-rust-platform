fb9bca679ab2a29915933f740e5207ee99f1309b
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-01-22 10:00:01 +0100

    Initialize structure with empty data (#211)

--- src/underscore_device/structure.rs
+++ src/underscore_device/structure.rs
@@ -5,21 +5,21 @@ pub mod instance;
 use instance::{Alert, InstanceElement};
 use panduza_platform_core::{instance::State, log_trace, Container, Error, Instance};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::collections::HashMap;
 
 use super::pack::InfoPack;
 
+/// Manage the Panduza core attributes '_/structure'
 ///
-/// Structure that represent the json maintained in '_/structure'
+/// The instance structure is represented by a json object
 ///
 #[derive(Default, Debug, Serialize, Deserialize)]
 pub struct Structure {
-    ///
     /// Instances managed
     ///
     driver_instances: HashMap<String, InstanceElement>,
 
-    ///
     /// User information about the structure
     ///
     info: Option<String>,
@@ -61,8 +61,7 @@ impl Structure {
     }
 }
 
-///
-///
+/// Mount the structure attribute and manage events
 ///
 pub async fn mount(mut instance: Instance, pack: InfoPack) -> Result<(), Error> {
     //
@@ -77,13 +76,21 @@ pub async fn mount(mut instance: Instance, pack: InfoPack) -> Result<(), Error> 
         .finish_as_json()
         .await?;
 
-    let pack_clone3 = pack.clone();
+    //
+    // Set the initial structure
+    structure_att
+        .set(pack.device_structure_as_json_value().await?)
+        .await?;
+
+    //
+    // Watch for structure changes
+    let pack_bis = pack.clone();
     instance
         .spawn("structure/watcher", async move {
             //
             //
-            let structure_change = pack_clone3.instance_structure_change_notifier().await;
-            // let pack_clone4 = pack_clone3.clone();
+            let structure_change = pack_bis.instance_structure_change_notifier().await;
+            // let pack_clone4 = pack_bis.clone();
 
             loop {
                 //
@@ -91,7 +98,7 @@ pub async fn mount(mut instance: Instance, pack: InfoPack) -> Result<(), Error> 
                 structure_change.notified().await;
                 log_trace!(logger, "structure change notification");
 
-                let structure = pack_clone3.device_structure_as_json_value().await.unwrap();
+                let structure = pack_bis.device_structure_as_json_value().await.unwrap();
                 log_trace!(logger, "new structure {:?}", structure);
 
                 structure_att.set(structure).await.unwrap();
